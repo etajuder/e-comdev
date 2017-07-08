c5ebc71c57553a3aa103c2a51286816b45211666
wapjude <wapjude@gmail.com>
2017-07-08 14:51:34 +0100

    fix picture changig

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-$('document').ready(function() {
-    alert('ready');
+
+function reLoadPlugin() {
+  $("#zoom_01").elevateZoom();
+}
+$("body").on("click", "#extra", function() {
+    var target = $("#zoom_01");
+    var ez =     $('#zoom_01').data('elevateZoom');
+    var src = $(this).attr('src');
+    target.attr("src", src);
+    target.attr('data-zoom-image', src);
+    ez.swaptheimage(src,src);
+    //reLoadPlugin();
 });
+
